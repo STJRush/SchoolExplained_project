1432d06c249b863e7441b6334bd886d587e8d909
OwenMacSau <16OMacNamara@stjosephsrush.ie>
2018-02-22 09:00:28 +0000

    Update SchooloExplained_V1.py

--- SchooloExplained_V1.py
+++ SchooloExplained_V1.py
@@ -1,1 +1,45 @@
+from time import sleep
+print("welcome to (pending title)!!")               #print("")
+sleep(1)
+print("as you probably allready know, this is a program to help you find inspiration in school and while doing homework by telling you what it is usefull for")
+sleep(1)
+print("so no furder addue, lets get into it!!!")
+sleep(1)
+print("here is an example on how to use it and how it works and looks like")
+sleep(1.5)
+print("for purposes of this example choose maths and algebra, ok?!")
+sleep(.5)
+print("lets start!!!")
 
+
+subjects=["Religion", "History", "Irish", "Science", "Maths", "English", "Geography", "Spanish", "German", "French", "Business", "Coding", "Homec", "Woodwork", "T.G", "P.E", "Art", "Music",]
+
+#EXAMPLE:
+print("what subject would you like to know about?")
+sleep(1)
+print("(remember to choose maths and algebra!!)")
+sleep(1)
+
+print("this is how the program looks like:")
+sleep(1)
+
+print("So, what subject do you want to learn about")
+subject=input()
+
+if subject=="Maths":
+    print("ok good!")
+
+else:
+    print("ERROR!")
+
+
+
+print("ok. so what topic do you want to know about")
+
+topic=input()
+
+if topic=="algebra":
+    print("ok well done!")
+
+else:
+    print("ERROR!")
